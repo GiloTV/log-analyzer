c2e80d96851e91fe1106e613ed3a2a178c5f1c6b
Galileo <150097868+GiloTV@users.noreply.github.com>
2026-09-02 19:53:08 -0600

    Build the log generator logic & generate the first log to analyze

--- log_generator.py
+++ log_generator.py
@@ -1,3 +1,8 @@
+import json
+import random
+
+log_levels = ["INFO", "WARNING", "ERROR"]
+
 messages = {
     "INFO": [
         "Server started successfully",
@@ -20,3 +25,17 @@ messages = {
         "Service unavailable"
     ]
 }
+
+file_path = "python_projects\Log-Analyzer\sample_logs\sample.log"
+
+
+try:
+    with open(file= file_path, mode="w") as file:
+        for i in range(50):
+            log_level_random = random.choice(log_levels)
+            log_message = random.choice(messages[log_level_random])
+            file.write(log_message+"\n")
+        #json.dump(messages, file, indent=4)
+        print(f"log file {file_path} was created!")
+except FileNotFoundError:
+    print("File was not found")
