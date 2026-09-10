687fa548c095230883758b1e4bebf6e076f93f1b
Galileo <150097868+GiloTV@users.noreply.github.com>
2026-09-09 19:12:30 -0600

    Prevent the log analyzer to crash on empty files and show the full list of errors inside the log files. Added another log file to test the log analyzer

--- log_analyzer.py
+++ log_analyzer.py
@@ -12,21 +12,23 @@ def log_reader(log_path):
 
 
 log_content = log_reader(cwd + "\sample_logs\sample1.log")
-info_message = log_content.count("INFO")
-warning_message = log_content.count("WARNING")
-error_message = log_content.count("ERROR")
-total_entries = info_message + warning_message + error_message
-log_entries = set(log_content.split("\n"))
 
-print(f"{"=" * 5} Log Entries {"=" * 5}\n")
-print(f"Total entries: {total_entries}")
-print(f"INFO: {info_message}")
-print(f"WARNING: {warning_message}")
-print(f"ERRORS: {error_message}")
-print(f"Error average {int(error_message/total_entries*100)}%")
-print(f"\n{"=" * 25}\n")
+if log_content:
+    info_message = log_content.count("INFO")
+    warning_message = log_content.count("WARNING")
+    error_message = log_content.count("ERROR")
+    total_entries = info_message + warning_message + error_message
+    log_entries = log_content.split("\n")
 
-print(f"{"="*6} ERRORS {"="*6}")
-for entry in log_entries:
-    if "ERROR" in entry:
-        print(entry)
+    print(f"{"=" * 5} Log Entries {"=" * 5}\n")
+    print(f"Total entries: {total_entries}")
+    print(f"INFO: {info_message}")
+    print(f"WARNING: {warning_message}")
+    print(f"ERRORS: {error_message}")
+    print(f"Error average {int(error_message/total_entries*100)}%")
+    print(f"\n{"=" * 25}\n")
+
+    print(f"{"="*6} ERRORS {"="*6}")
+    for entry in log_entries:
+        if "ERROR" in entry:
+            print(entry)
